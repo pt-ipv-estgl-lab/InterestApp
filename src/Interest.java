b882ad1e833d22d338583a2c7407345b5344700c
Costa, C. J. <ccosta@estgl.ipv.pt>
2024-03-05 10:15:49 +0000

    Interest Version 2

--- src/Interest.java
+++ src/Interest.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 /**
  * This class implements a simple program that
  * will compute the amount of interest that is
- * earned on $17,000 invested at an interest
- * rate of 0.027 for one year. The interest and
+ * readed, invested at an interest
+ * rate, also readed, for one year. The interest and
  * the value of the investment after one year are
  * printed to standard output.
  */
@@ -11,16 +13,22 @@ public class Interest {
     public static void main(String[] args) {
         /* Declare the variables. */
 
+        Scanner stdin = new Scanner(System.in); // Create a Scanner on keyboard
+
         double principal; // The value of the investment.
         double rate; // The annual interest rate.
         double interest; // Interest earned in one year.
 
         System.out.println("Interest Computing");
+        // Data input
+        System.out.print("Enter the initial investment: ");
+        principal = stdin.nextDouble();
+
+      System.out.print("Enter the annual interest rate (as a decimal): ");
+      rate = stdin.nextDouble();        
 
         /* Do the computations. */
 
-        principal = 17000;
-        rate = 0.027;
         interest = principal * rate; // Compute the interest.
 
         principal = principal + interest;
@@ -29,9 +37,9 @@ public class Interest {
 
         /* Output the results. */
 
-        System.out.print("The interest earned is $");
-        System.out.println(interest);
-        System.out.print("The value of the investment after one year is $");
-        System.out.println(principal);
+        System.out.println("The interest earned is $"+ interest);
+        System.out.printf("The value of the investment after one year is $%1.2f\n",
+            principal);
+        stdin.close(); // close Scanner on keyboard
     }
 }
